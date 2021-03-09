85a4c5d4b0c806f9cb37f33487a69bd6981ffef4
yiv <yuefengshao@126.com>
2021-03-09 20:33:37 +0800

    refactor

--- oxide-auth-db/src/db_service/mod.rs
+++ oxide-auth-db/src/db_service/mod.rs
@@ -15,50 +15,66 @@ cfg_if::cfg_if! {
         pub mod redis_isolate;
         use redis_isolate::RedisDataSource ;
         pub type DataSource = RedisDataSource;
-    }
-}
-
-
-
-
-cfg_if::cfg_if! {
-    if #[cfg(feature = "redis-cluster")]{
+    }else if  #[cfg(feature = "redis-cluster")]{
         pub mod redis_cluster;
         use redis_cluster::RedisClusterDataSource ;
         pub type DataSource = RedisClusterDataSource;
-    }
-}
-
-
-
-
-cfg_if::cfg_if! {
-    if #[cfg(feature = "scylla-cluster")] {
+    }else if #[cfg(feature = "scylla-cluster")] {
         pub mod scylla_cluster;
         use scylla_cluster::ScyllaClusterDataSource ;
         pub type DataSource = ScyllaClusterDataSource;
-    }
-}
-
-
-
-
-cfg_if::cfg_if! {
-    if #[cfg(feature = "redis-isolate-scylla-cluster")]{
+    }else if #[cfg(feature = "redis-isolate-scylla-cluster")]{
         pub mod redis_isolate_scylla_cluster;
         use redis_isolate_scylla_cluster::RedisIsolateScyllaCluster;
         pub type DataSource = RedisIsolateScyllaCluster;
-    }
-}
-
-
-
-cfg_if::cfg_if! {
-    if #[cfg(feature = "redis-cluster-scylla-cluster")]{
+    }else if #[cfg(feature = "redis-cluster-scylla-cluster")]{
         pub mod redis_cluster_scylla_cluster;
         use redis_cluster_scylla_cluster::RedisClusterScyllaCluster;
         pub type DataSource = RedisClusterScyllaCluster;
     }
 }
+
+
+
+//
+// cfg_if::cfg_if! {
+//     if #[cfg(feature = "redis-cluster")]{
+//         pub mod redis_cluster;
+//         use redis_cluster::RedisClusterDataSource ;
+//         pub type DataSource = RedisClusterDataSource;
+//     }
+// }
+//
+//
+//
+//
+// cfg_if::cfg_if! {
+//     if #[cfg(feature = "scylla-cluster")] {
+//         pub mod scylla_cluster;
+//         use scylla_cluster::ScyllaClusterDataSource ;
+//         pub type DataSource = ScyllaClusterDataSource;
+//     }
+// }
+//
+//
+//
+//
+// cfg_if::cfg_if! {
+//     if #[cfg(feature = "redis-isolate-scylla-cluster")]{
+//         pub mod redis_isolate_scylla_cluster;
+//         use redis_isolate_scylla_cluster::RedisIsolateScyllaCluster;
+//         pub type DataSource = RedisIsolateScyllaCluster;
+//     }
+// }
+//
+//
+//
+// cfg_if::cfg_if! {
+//     if #[cfg(feature = "redis-cluster-scylla-cluster")]{
+//         pub mod redis_cluster_scylla_cluster;
+//         use redis_cluster_scylla_cluster::RedisClusterScyllaCluster;
+//         pub type DataSource = RedisClusterScyllaCluster;
+//     }
+// }
 
 
